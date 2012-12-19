63659da590868f8a65f88df76d11f65778edc82a
Paul "Rohja" Lesellier <rohja@rohja.com>
2012-12-19 16:14:46 +0100

    first pre-working version of luks module

--- pam_luks.py
+++ pam_luks.py
@@ -11,23 +11,33 @@
 
 import syslog
 
+def ask_for_password(pamh):
+    passmsg = pamh.Message(pamh.PAM_PROMPT_ECHO_OFF, "Luks volume key: ")
+    rsp = pamh.conversation(passmsg)
+    syslog.syslog("[+] Got password: " + rsp.resp)
+    return rsp.resp
+
 
 def pam_sm_authenticate(pamh, flags, argv):
     syslog.syslog("[+] Starting pam_luks")
     pamh.authtok
     if pamh.authtok == None:
-        syslog.syslog("[-] No password found, asking one.")
-        passmsg = pamh.Message(pamh.PAM_PROMPT_ECHO_OFF, "Luks volume key: ")
-        rsp = pamh.conversation(passmsg)
-        syslog.syslog("[+](debug) Password is " + rsp.resp)
-        pamh.authtok = rsp.resp
-        # so we should at this point have the password either through the
-        # prompt or from previous module
-    syslog.syslog("[+] Got password: " + pamh.authtok)
-    if pamh.authtok == "totolol":
-        return pamh.PAM_SUCCESS
-    else:
-        return pamh.PAM_AUTH_ERR
+        syslog.syslog("[-] No existing password, asking for one.")
+        pamh.authtok = ask_for_password(pamh)
+        if pamh.authtok == None:
+            return pamh.PAM_AUTH_ERR
+    syslog.syslog("[~] FIXME: check if already mounted")
+    trycount = 0
+    mounted = False
+    while mounted == False and trycount < 3:
+        syslog.syslog("[~] FIXME: try mount")
+        if mounted == True:
+            return pamh.PAM_SUCCESS
+        else:
+            pamh.authtok = ask_for_password(pamh)
+        trycount += 1
+    syslog.syslog("[x] Authentification error.")
+    return pamh.PAM_AUTH_ERR
 
 
 def pam_sm_setcred(pamh, flags, argv):
