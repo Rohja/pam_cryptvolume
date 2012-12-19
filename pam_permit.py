db589e01b21bfa16dbcf1351eac1b095a2b6a487
Paul "Rohja" Lesellier <rohja@rohja.com>
2012-12-19 11:52:45 +0100

    pam_permit.py script used to try pam functions

--- pam_permit.py
+++ pam_permit.py
@@ -2,14 +2,23 @@
 # Duplicates pam_permit.create#
 DEFAULT_USER    = "nobody"
 
+import syslog
+
 def pam_sm_authenticate(pamh, flags, argv):
-  print "pam_sm_authenticate()"
+  # INFO MESSAGE
+  # try:
+  #   pamh.conversation(pamh.Message(pamh.PAM_TEXT_INFO, "pam_sm_authenticate()"))
+  # except pamh.exception:
+  #   return pamh.PAM_SYSTEM_ERR
+  syslog.syslog("pam_sm_authenticate() executed from pam_python.so")
+  # ChECKING STUFF
   try:
     user = pamh.get_user(None)
   except pamh.exception, e:
     return e.pam_result
   if user == None:
-    pam.user = DEFAULT_USER  return pamh.PAM_SUCCESS
+    pam.user = DEFAULT_USER
+  return pamh.PAM_SUCCESS
 
 def pam_sm_setcred(pamh, flags, argv):
   print "pam_sm_setcred()"
@@ -37,8 +46,8 @@ Assuming it and pam_python.so are in the directory
   /lib/security adding these rules to
   /etc/pam.conf would run it:
 
-login account   requisite   pam_python.so pam_accept.python
-login auth      requisite   pam_python.so pam_accept.python
-login password  requisite   pam_python.so pam_accept.python
+login account   requisite   pam_python.so pam_accept.py
+login auth      requisite   pam_python.so pam_accept.py
+login password  requisite   pam_python.so pam_accept.py
 login session   requisite   pam_python.so pam_accept.py
 '''
